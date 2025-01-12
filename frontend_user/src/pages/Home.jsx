65b1d35ba141c7cf65e2c23f3955bafbb68d5283
Thao <nguyenthiphuongthao_t67@hus.edu.vn>
2025-01-13 06:40:13 +0700

    add pages

---
+++ frontend_user/src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import Header from '../components/Header/Header';
+import Hero from '../components/Hero/Hero';
+import MenuExplorer from '../components/Menu/MenuExplorer';
+
+const Home = () => {
+  return (
+    <>
+      <Header />
+      <Hero />
+      <MenuExplorer />
+    </>
+  );
+};
+
+export default Home;
